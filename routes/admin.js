37dd93881092e2e45bdebe5a64f20214fade5185
w-udhav <udhavwadhawan@hotmail.com>
2023-07-20 23:21:13 +0530

    update: admin save api

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,27 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
-const AdminModel = require("../models/admin.js")
+const AdminModel = require("../models/admin.js");
+const StudentModel = require("../models/student.js");
 
+router.get("/", (req, res) => {
+  res.send("You'r in admin");
+});
+router.post("/login", (req, res) => {
+  const data = req.body;
+  AdminModel(data);
+});
 
-router.get("/", (req, res)=>{
-    res.send("You'r in admin")
-})
-router.post("/login", (req, res)=>{
+router.post("/save", (req, res) => {
+  try {
     const data = req.body;
-    AdminModel(data)
-})
+    const student = new StudentModel(data);
+    student.save();
+    res.status(200).json({
+      message: "Data received",
+    });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
 
 module.exports = router;
